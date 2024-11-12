06e2b5ce8eb8a070e5344ef84ebc4ba232017ab5
Leande-R <leander.robin.02@gmail.com>
2024-11-12 19:13:52 +0100

    CreateChargerStepdefinitions changed

--- ProjectEdison/src/test/java/org/example/CreateChargerStepdefinitions.java
+++ ProjectEdison/src/test/java/org/example/CreateChargerStepdefinitions.java
@@ -7,60 +7,61 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CreateChargerStepdefinitions {
 
-
-
-    private Charger charger;
+    private Charger charger = new Charger();
     private ChargingStation chargingStation;
-
 
     @ParameterType("AC|DC")
     public ChargingMethod method(String method) {
         return ChargingMethod.valueOf(method);
     }
 
-    @Given("I created a new charging station in Fürstenfeld")
-    public void iCreatedANewChargingStationInFürstenfeld() {
-        chargingStation = new ChargingStation("Einsteingasse 1, 8280", "Fürstenfeld Charging", 0.25, 0.35);
+
+    @Given("I already created a charging station in Fürstenfeld called {string}")
+    public void iAlreadyCreatedAChargingStationInFürstenfeldCalled(String givenLocation) {
+        this.chargingStation = new ChargingStation("Einsteingasse 1, 8280", givenLocation, 0.25, 0.35);
     }
-
-
-    @Given("I want to create a new charger")
-    public void iWantToCreateANewCharger() {
-        charger = new Charger( 0, null, null, null);
-    }
-
 
 
     @When("I assign the charger to the charging station {string}")
-    public void iAssignTheChargerToTheChargingStation(String location) {
-        charger.setChargingStation(chargingStation);
+    public void iAssignTheChargerToTheChargingStation(String givenLocation) {
+        if (chargingStation.getLocation().equals(givenLocation)) {
+            charger.setChargingStation(chargingStation);
+        } else {
+            throw new IllegalArgumentException("Charging Station not found");
+        }
     }
+
 
     @And("I enter the ID {int}")
     public void iEnterTheID(int id) {
         charger.setID(id);
     }
 
-    @And("I select the Charging Method as {method}")
-    public void iSelectTheChargingMethodAs(ChargingMethod method) {
-        charger.setChargingMethod(method);
 
+    @And("I select the Charging Method as {string}")
+    public void iSelectTheChargingMethodAs(String method) {
+        ChargingMethod cMethod = ChargingMethod.valueOf(method);
+        charger.setChargingMethod(cMethod);
     }
 
-   /* @Then("a new Charger with the ID {int} assigned to the Charging Station {String} and the Charging Method {method} is created")
-    public void aNewChargerIsCreated(int id, String chargingstation, ChargingMethod method) {
-        assertEquals(id, charger.getID());
-        assertEquals(chargingstation, charger.getChargingStation().getLocation());
-        assertEquals(method, charger.getChargingMethod());
 
-    }*/
-
-    @Then("a new Charger with the ID {int} assigned to the Charging Station {string} is created")
-    public void aNewChargerWithTheIDAssignedToTheChargingStationAndTheChargingMethodDCIsCreated(int id, String chargingstation) {
-        assertEquals(id, charger.getID());
-        assertEquals(chargingstation, charger.getChargingStation().getLocation());
+    @Then("a new Charger with the ID {int} is created")
+    public void aNewChargerWithTheIDIsCreated(int identification) {
+        assertEquals(identification, charger.getID());
     }
 
+
+    @And("it has the Charging Method {string}")
+    public void itHasTheChargingMethod(String chargingMethodString) {
+        ChargingMethod chargingMethod = ChargingMethod.valueOf(chargingMethodString);
+        assertEquals(chargingMethod, charger.getChargingMethod());
+    }
+
+
+    @And("it's assigned to the Charging Station {string}")
+    public void itSAssignedToTheChargingStation(String givenLocation) {
+        assertEquals(givenLocation, charger.getChargingStation().getLocation());
+    }
 
 
 }
